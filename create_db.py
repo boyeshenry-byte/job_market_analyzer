a3824751bdb7f9ba694c4b4a3f294543d00a341f
Henry Boyes <boyeshenry@gmail.com>
2026-02-12 09:32:14 -0500

    Refactored create_db.py into function

--- create_db.py
+++ create_db.py
@@ -1,25 +1,59 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("sqlite:///data/jobs.db")
+def make_db():
+    """
+    Creates a database to store data called from job scrapers.
 
-with engine.connect() as conn:
-    conn.execute(text("""
-        CREATE TABLE IF NOT EXISTS jobs (
-            title TEXT,
-            company TEXT,
-            location TEXT,
-            salary_min REAL,
-            salary_max REAL,
-            salary_range TEXT,
-            tags TEXT,
-            job_type TEXT,
-            region TEXT,
-            date TEXT,
-            url TEXT,
-            source TEXT,
-            unique(title, company, source)       
-        )
-    """))
-    conn.commit()
+    : attributes :
+    
+    title : str
+        the name of the position
+    company : str
+        the company the position is with
+    location : str
+        the location of the job
+    salary_min : int
+        the minimum salary for the position
+    salary_max : int
+        the maximum salary for the position
+    salary_range : int
+        the salary range for the position (if min and max not used)
+    tags : str
+        the tags related to the job
+    job_type: str
+        the type of posititon (full-time, part-time, contract, freelance)
+    region : str
+        the region of the company
+    date : str
+        the date the job was listed
+    url : str
+        the url for the job (if provided)
+    source : str
+        which job board the job is listed on
+    
+    """
+    engine = create_engine("sqlite:///data/jobs.db")
 
-print("Database created successfully")
+    with engine.connect() as conn:
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS jobs (
+                title TEXT,
+                company TEXT,
+                location TEXT,
+                salary_min REAL,
+                salary_max REAL,
+                salary_range TEXT,
+                tags TEXT,
+                job_type TEXT,
+                region TEXT,
+                date TEXT,
+                url TEXT,
+                source TEXT,
+                unique(title, company, source)       
+            )
+        """))
+        conn.commit()
+
+if __name__ == '__main__':
+    make_db()
+    print("Database created successfully")
